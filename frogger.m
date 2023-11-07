a2206f8d3d54c2f1d39d890be612fce30646b8b5
SohumSKS <37936322+sohumsuthar@users.noreply.github.com>
2023-11-07 00:25:48 -0500

    okok

--- frogger.m
+++ frogger.m
@@ -4,7 +4,7 @@ close all
 
 
 scn = simpleGameEngine('frogger.png', 16, 16, 5, [0,0,0]);
-frogPos = [1, 2];
+frogPos = [4, 2];
 sceneMatrix = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1; ...
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1; ...
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1; ...
@@ -19,12 +19,25 @@ sceneMatrix(frogPos(1), frogPos(2)) = 2;
 while true
     drawScene(scn,sceneMatrix);
     k = getKeyboardInput(scn);
-    if k == 'uparrow'
+    if strcmp(k, 'uparrow') && (frogPos(1) ~= 1)
+        sceneMatrix(frogPos(1), frogPos(2)) = 1;
+        frogPos(1) = frogPos(1) - 1;
+        sceneMatrix(frogPos(1), frogPos(2)) = 2;
+    elseif strcmp(k, 'downarrow') && (frogPos(1) ~= 9)
+        sceneMatrix(frogPos(1), frogPos(2)) = 1;
+        frogPos(1) = frogPos(1) + 1;
+        sceneMatrix(frogPos(1), frogPos(2)) = 2;
+    elseif strcmp(k, 'rightarrow') && (frogPos(2) ~= 11)
         sceneMatrix(frogPos(1), frogPos(2)) = 1;
         frogPos(2) = frogPos(2) + 1;
         sceneMatrix(frogPos(1), frogPos(2)) = 2;
-    end
+    elseif strcmp(k, 'leftarrow') && (frogPos(2) ~= 1)
+        sceneMatrix(frogPos(1), frogPos(2)) = 1;
+        frogPos(2) = frogPos(2) - 1;
+        sceneMatrix(frogPos(1), frogPos(2)) = 2;
 
+    end
+   pause(0.1);
 end
 
 

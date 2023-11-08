6fe2e370f6b60c6fb836c615d5a26772d8697908
Opal1111 <opallondraville@gmail.com>
2023-11-08 09:31:01 -0500

    updated function

--- moveCars.m
+++ moveCars.m
@@ -1,7 +1,7 @@
-function [carPosition1,carPosition2] = (,)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
- carPosition1 = [7, 8, 9];
- carPosition2 = ;
+function [carPosition1,carPosition2] = moveCar(c1)
+% Take the postion of car and add 1 to make it move across the screen
+% c1 = [7, 8, 9] 
+carPosition1 = c1 + 1;
+carPosition2 = carPosition1 + 1;
 
 end

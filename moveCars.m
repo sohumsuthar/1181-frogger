0f44a81b842cd575d87b3ad451e34bbb6c484cdc
Opal1111 <opallondraville@gmail.com>
2023-11-08 09:34:02 -0500

    Update

--- moveCars.m
+++ moveCars.m
@@ -1,5 +1,6 @@
 function [carPosition1,carPosition2] = moveCar(c1)
 % Take the postion of car and add 1 to make it move across the screen
+% Make an if statement to move the cars 1 over, but then say if it goes off the board start back at the beginning?
 % c1 = [7, 8, 9] 
 carPosition1 = c1 + 1;
 carPosition2 = carPosition1 + 1;

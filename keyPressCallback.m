1f02840c87bd0930e3bd936d8c63ed5fd7266008
Opal1111 <opallondraville@gmail.com>
2023-11-20 08:21:05 -0500

    jfdklsa

--- keyPressCallback.m
+++ keyPressCallback.m
@@ -9,32 +9,32 @@ global f;
 global bushPos;
 
 k = event.Key;
-
-if strcmp(k, 'uparrow') && (frogPos(1) ~= 1) && ~((ismember(frogPos(2), bushPos) && frogPos(1) == 2))
-    % Move the frog up
-    blankL(frogPos(1), frogPos(2)) = 1;
-    frogPos(1) = frogPos(1) - 1;
-    blankL(frogPos(1), frogPos(2)) = 2;
-    gameEndCheck();
-    refreshScene();
-elseif strcmp(k, 'downarrow') && (frogPos(1) ~= 11)
-    % Move the frog downe
-    blankL(frogPos(1), frogPos(2)) = 1;
-    frogPos(1) = frogPos(1) + 1;
-    blankL(frogPos(1), frogPos(2)) = 2;
-    refreshScene();
-elseif strcmp(k, 'rightarrow') && (frogPos(2) ~= 11)
-    % Move the frog right
-    blankL(frogPos(1), frogPos(2)) = 1;
-    frogPos(2) = frogPos(2) + 1;
-    blankL(frogPos(1), frogPos(2)) = 2;
-    refreshScene();
-elseif strcmp(k, 'leftarrow') && (frogPos(2) ~= 1)
-    % Move the frog left
-    blankL(frogPos(1), frogPos(2)) = 1;
-    frogPos(2) = frogPos(2) - 1;
-    blankL(frogPos(1), frogPos(2)) = 2;
-    refreshScene();
+switch k
+    case strcmp(k, 'uparrow') && (frogPos(1) ~= 1) && ~((ismember(frogPos(2), bushPos) && frogPos(1) == 2))
+        % Move the frog up
+        blankL(frogPos(1), frogPos(2)) = 1;
+        frogPos(1) = frogPos(1) - 1;
+        blankL(frogPos(1), frogPos(2)) = 2;
+        gameEndCheck();
+        refreshScene();
+    case strcmp(k, 'downarrow') && (frogPos(1) ~= 11)
+        % Move the frog downe
+        blankL(frogPos(1), frogPos(2)) = 1;
+        frogPos(1) = frogPos(1) + 1;
+        blankL(frogPos(1), frogPos(2)) = 2;
+        refreshScene();
+    case strcmp(k, 'rightarrow') && (frogPos(2) ~= 11)
+        % Move the frog right
+        blankL(frogPos(1), frogPos(2)) = 1;
+        frogPos(2) = frogPos(2) + 1;
+        blankL(frogPos(1), frogPos(2)) = 2;
+        refreshScene();
+    case strcmp(k, 'leftarrow') && (frogPos(2) ~= 1)
+        % Move the frog left
+        blankL(frogPos(1), frogPos(2)) = 1;
+        frogPos(2) = frogPos(2) - 1;
+        blankL(frogPos(1), frogPos(2)) = 2;
+        refreshScene();
 end
 
 end

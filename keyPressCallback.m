557deec55cea1317529f7ecaa4f7aef08013f352
Opal1111 <opallondraville@gmail.com>
2023-11-20 08:29:06 -0500

    adf

--- keyPressCallback.m
+++ keyPressCallback.m
@@ -9,32 +9,40 @@ global f;
 global bushPos;
 
 k = event.Key;
-switch k
-    case strcmp(k, 'uparrow') && (frogPos(1) ~= 1) && ~((ismember(frogPos(2), bushPos) && frogPos(1) == 2))
+switch (k)
+    case 'uparrow' 
+        if (frogPos(1) ~= 1) && ~((ismember(frogPos(2), bushPos) && frogPos(1) == 2))
         % Move the frog up
         blankL(frogPos(1), frogPos(2)) = 1;
         frogPos(1) = frogPos(1) - 1;
         blankL(frogPos(1), frogPos(2)) = 2;
         gameEndCheck();
         refreshScene();
-    case strcmp(k, 'downarrow') && (frogPos(1) ~= 11)
+        end
+    case 'downarrow'
+        if (frogPos(1) ~= 11)
         % Move the frog downe
         blankL(frogPos(1), frogPos(2)) = 1;
         frogPos(1) = frogPos(1) + 1;
         blankL(frogPos(1), frogPos(2)) = 2;
         refreshScene();
-    case strcmp(k, 'rightarrow') && (frogPos(2) ~= 11)
+        end
+    case 'rightarrow'
+        if (frogPos(2) ~= 11)
         % Move the frog right
         blankL(frogPos(1), frogPos(2)) = 1;
         frogPos(2) = frogPos(2) + 1;
         blankL(frogPos(1), frogPos(2)) = 2;
         refreshScene();
-    case strcmp(k, 'leftarrow') && (frogPos(2) ~= 1)
+        end
+    case 'leftarrow' 
+        if (frogPos(2) ~= 1)
         % Move the frog left
         blankL(frogPos(1), frogPos(2)) = 1;
         frogPos(2) = frogPos(2) - 1;
         blankL(frogPos(1), frogPos(2)) = 2;
         refreshScene();
+        end 
 end
-
+pause(.5)
 end

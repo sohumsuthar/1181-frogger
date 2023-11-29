98d794f8ccb634536dc0c695239fa97d4ef9a1ca
Opal1111 <opallondraville@gmail.com>
2023-11-29 08:20:54 -0500

    update

--- letterIndex.m
+++ letterIndex.m
@@ -1,4 +1,4 @@
-function [outputArg1,outputArg2] = untitled(inputArg1,inputArg2)
+function letterIndex = untitled(inputArg1,inputArg2)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 outputArg1 = inputArg1;

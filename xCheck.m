fc8ef04f13312bdff2b30ad942289244d6a61527
sohumsuthar <37936322+sohumsuthar@users.noreply.github.com>
2023-11-29 20:44:32 -0500

    tgyitgyuijighghuji

--- xCheck.m
+++ xCheck.m
@@ -2,7 +2,9 @@ function xCheck()
 global topL;
 global frogPos;
 global enemies;
-            if (ismember(topL(frogPos(1), frogPos(2)), enemies)) || ((frogPos(1) >= 2 && frogPos(1) <= 5) && topL(frogPos(1), frogPos(2)) == 1);
+            if (ismember(topL(frogPos(1), frogPos(2)), enemies)) || ...
+                    ((frogPos(1) >= 2 && frogPos(1) <= 5) && ...
+                    topL(frogPos(1), frogPos(2)) == 1)
 
                 resetFrogPos(true);
             end

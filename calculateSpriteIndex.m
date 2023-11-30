95f546ef4582cf60bec3cd0e7e9d8fd104ede06b
sohumsuthar <37936322+sohumsuthar@users.noreply.github.com>
2023-11-29 22:22:26 -0500

    delteeded asdfjahsdklfd fasjkl;wsdf h jklasdflhjkasdf

--- calculateSpriteIndex.m
+++
@@ -1,8 +0,0 @@
-function spriteIndex = calculateSpriteIndex(spriteNumber)
-    % Implement your logic here to determine the sprite index
-    % For example, you might cycle through a list of sprite indices.
-    
-    % For simplicity, let's assume we cycle through sprites 1 to 3.
-    numSprites = 3;
-    spriteIndex = mod(spriteNumber, numSprites) + 1;
-end

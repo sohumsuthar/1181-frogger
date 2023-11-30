af0a8cd31ef24d3c486cb4551c9fa6e35d8920a7
sohumsuthar <37936322+sohumsuthar@users.noreply.github.com>
2023-11-29 20:10:41 -0500

    wasd

--- keyPressCallback.m
+++ keyPressCallback.m
@@ -6,44 +6,42 @@ global bushPos;
 global lives;
 global enemies;
 
-enemy = (ismember(topL(frogPos(1), frogPos(2)), enemies)) || ((frogPos(1) >= 2 && frogPos(1) <= 5) && topL(frogPos(1), frogPos(2)) == 1);
-
 % Set up, down, right and left arrow keys to the corresponding movement of the frog
 % Refresh scene and/or frog position after each iteration
 k = event.Key;
 switch (k)
-    case 'uparrow'
+    case {'uparrow', 'w'}
         if (frogPos(1) ~= 1) && ~((ismember(frogPos(2), bushPos) && frogPos(1) == 2))
             blankL(frogPos(1), frogPos(2)) = 1;
             frogPos(1) = frogPos(1) - 1;
             blankL(frogPos(1), frogPos(2)) = 2;
-xCheck();
+            xCheck();
             gameEndCheck();
             refreshScene();
         end
-    case 'downarrow'
+    case {'downarrow', 's'}
         if (frogPos(1) ~= 11)
             blankL(frogPos(1), frogPos(2)) = 1;
             frogPos(1) = frogPos(1) + 1;
             blankL(frogPos(1), frogPos(2)) = 2;
-xCheck();
+            xCheck();
             refreshScene();
         end
-    case 'rightarrow'
+    case {'rightarrow', 'd'}
         if (frogPos(2) ~= 11)
 
             blankL(frogPos(1), frogPos(2)) = 1;
             frogPos(2) = frogPos(2) + 1;
             blankL(frogPos(1), frogPos(2)) = 2;
-xCheck();
+            xCheck();
             refreshScene();
         end
-    case 'leftarrow'
+    case {'leftarrow', 'a'}
         if (frogPos(2) ~= 1)
             blankL(frogPos(1), frogPos(2)) = 1;
             frogPos(2) = frogPos(2) - 1;
             blankL(frogPos(1), frogPos(2)) = 2;
-xCheck();
+            xCheck();
             refreshScene();
         end
     case 'l'

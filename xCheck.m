9fdda1860e66f9da2d78ac1fe6f2293fe347af36
sohumsuthar <37936322+sohumsuthar@users.noreply.github.com>
2023-12-01 03:01:25 -0500

    COMMENTS

--- xCheck.m
+++ xCheck.m
@@ -1,17 +1,21 @@
+% function to check whether the frog dies or not
 function xCheck()
+
+% bring global variables to workspace
 global topL;
 global frogPos;
 global enemies;
 
+% read audio files for frog death sounds
 [y1, Fs1] = audioread('sound-frogger-squash.wav');
 [y2, Fs2] = audioread('sound-frogger-plunk.wav');
 
 % Reset the frog's position if it gets hit by an enemy
-if (ismember(topL(frogPos(1), frogPos(2)), enemies))
-    sound(y1, Fs1);
-    resetFrogPos(true);
-elseif ((frogPos(1) >= 2 && frogPos(1) <= 5) && topL(frogPos(1), frogPos(2)) == 1)
-    sound(y2, Fs2)
-    resetFrogPos(true);
+if (ismember(topL(frogPos(1), frogPos(2)), enemies)) % if the frog position in the top layer is an enemy sprite:
+    sound(y1, Fs1); % play death sound
+    resetFrogPos(true); % loss is true
+elseif ((frogPos(1) >= 2 && frogPos(1) <= 5) && topL(frogPos(1), frogPos(2)) == 1) % if the frog is in the water
+    sound(y2, Fs2) % play death sound for water
+    resetFrogPos(true); 
 end
 end

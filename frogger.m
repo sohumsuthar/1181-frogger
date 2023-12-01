b42aecba8609e403dd68ff17f172d11b238ae8d2
sohumsuthar <37936322+sohumsuthar@users.noreply.github.com>
2023-11-30 22:32:00 -0500

    fsgdfgsdfg

--- frogger.m
+++ frogger.m
@@ -1,6 +1,8 @@
 clc
 clear
 close all
+clear sound
+clear player
 
 global speed;
 global gameOver;
@@ -9,14 +11,24 @@ global Timer;
 initVars();
 startScreen();
 
+[y, Fs] = audioread('kensample.mp3');
+volume = 0.4;
+player = audioplayer(volume * y, Fs);
+
+
 % Add a pause to refresh scene and sprite movement
 while true
 tic;
+play(player);
 while ~gameOver
+    if ~isplaying(player)
+        play(player);
+    end
     movementSpriteControl();
     refreshScene();
     pause(speed);
 end
+
 Timer = toc;
 restartGame();
 end
